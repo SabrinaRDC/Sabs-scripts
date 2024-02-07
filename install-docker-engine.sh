07f618817de0929cd59c6d7a27c3e922102fc3bb
Sabrina <74781086+SabrinaRDC@users.noreply.github.com>
2024-02-06 22:31:46 -0800

    Update install-docker-engine.sh
    -y

--- install-docker-engine.sh
+++ install-docker-engine.sh
@@ -27,7 +27,7 @@ sudo apt update
 
 # Install latest
 echo -e "${ICyan}Installing Docker${ResetColor}"
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 echo -e "${IGreen}Docker Engine Installed${ResetColor}"
 
 # Install fuse-overlayfs
